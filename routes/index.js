67dab8614b4f04d4016175c822684f47ad114bfd
Sergey Romanyuk <Sergey.Romanyuk@dataart.com>
2017-02-05 21:03:14 +0200

    Up time added to home page(debug)

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,30 @@
 var express = require('express');
-var sys = require('sys');
-var shell = require('shelljs');
+
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var temp = shell.exec('cat /sys/class/thermal/thermal_zone0/temp', {silent: true}).output;
-    console.log('Temperature:', temp);
-    var output = shell.exec('netstat -rn', {silent: true}).output;
-    console.log(output);
-    var uptime = shell.exec('uptime', {silent: true}).output;
-    console.log('Uptime:', uptime);
-
+    var temp = "no data";
+    var upTime = "no data";
     var exec = require('child_process').exec;
 
-    function LogMe(error, stdout, stderr) {
-        console.log('Uptime 1:', stdout);
+    function getTemp(error, stdout, stderr) {
+        console.log('Temp 1:', stdout);
+        temp = stdout;
     }
 
-    exec("uptime", LogMe);
+    function getUptime(error, stdout, stderr) {
+        console.log('Uptime 1:', stdout);
+        upTime = stdout;
+    }
+
+    exec("uptime", getTemp());
+    exec("cat /sys/class/thermal/thermal_zone0/temp", getUptime());
     res.render('index', {
         title: 'Express',
         temp: temp,
-        uptime: uptime
+        uptime: upTime
     });
 });
 
