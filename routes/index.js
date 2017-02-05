12aa7e3ab0198de58b69d358752b2a2afe80fde0
Sergey Romanyuk <Sergey.Romanyuk@dataart.com>
2017-02-05 20:18:35 +0200

    Up time added to home page

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var temp = exec('cat /sys/class/thermal/thermal_zone0/temp', {silent: true}).output;
+    var uptime = exec('uptime', {silent: true}).output;
     res.render('index', {
         title: 'Express',
-        temp: temp
+        temp: temp,
+        uptime: uptime
     });
 });
 
