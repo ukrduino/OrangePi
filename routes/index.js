daeb17a086308794eebcef3ea0d57d16f185df21
Sergey Romanyuk <Sergey.Romanyuk@dataart.com>
2017-02-05 20:09:55 +0200

    Temp indication added to page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+require('shelljs/global');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    var temp = exec('cat /sys/class/thermal/thermal_zone0/temp', {silent: true}).output;
+    res.render('index', {
+        title: 'Express',
+        temp: temp
+    });
 });
 
 module.exports = router;
